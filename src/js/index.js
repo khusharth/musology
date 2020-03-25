8f7cec97ec4f3e35c8b7f32d38f6e57dfe931e35
Khusharth <khusharth19@gmail.com>
2020-03-26 01:03:04 +0530

    Added search boxes

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import "../scss/main.scss";
 import "../img/Logo.png";
-import "../img/favicon.png"
-import "../img/search.svg"
+import "../img/favicon.png";
+import "../img/search.svg";
+import "../img/chain.jpg";
 
 console.log("Hello World!");
