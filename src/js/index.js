301ce2755375ec71dc2447d60c842fac5cca6686
Khusharth <khusharth19@gmail.com>
2020-03-26 15:02:19 +0530

    Added pagination buttons and music details section

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
 import "../scss/main.scss";
 import "../img/Logo.png";
 import "../img/favicon.png";
-import "../img/search.svg";
+import "../img/sprite.svg";
 import "../img/chain.jpg";
+import "../img/big.jpg";
 
 console.log("Hello World!");
